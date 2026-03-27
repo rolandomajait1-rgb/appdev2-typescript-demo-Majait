ea3e4d0d14d413f585aa97acfe67f2569c9039f0
rolandomajait1-rgb <rolandomajait1@gmail.com>
2026-03-27 14:01:57 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,7 @@
-let userName1 = "Elmer";
+let userName1 = "Rolando";
+let userAge = 16;
+let userCourse = "BSIS";
 
 console.log(typeof userName1)
+console.log(typeof userAge)
+console.log(typeof userCourse)
