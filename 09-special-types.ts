12cdd3cb5c1ac06fcf4fc7577ae95dc62ceefb78
rolandomajait1-rgb <rolandomajait1@gmail.com>
2026-03-27 16:08:57 +0800

    Done playing and updating the 09-special-types.ts

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -3,11 +3,14 @@ let a: null;
 
 a = null
 // a = "Hi"
+console.log(a)
 
 let b: null | string
 
 b = null
+console.log(b)
 b = "Hi"
+console.log(b)
 // b = 10
 
 // Special Type - undefined
@@ -16,10 +19,14 @@ let c: undefined;
 c = undefined
 // c = null
 // c = "Hi"
+console.log(c)
+
 
 let d: undefined | string
 
 d = undefined
+console.log(d)
 d = "Hi"
 // d = null
 // d = 10
+console.log(d)
